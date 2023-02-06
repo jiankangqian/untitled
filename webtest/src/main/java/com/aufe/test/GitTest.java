b35c587cc6475e0483b4cdc5efc0241b7f13cc18
health <20184031@aufe.edu.com>
2023-02-06 16:35:13 +0800

    second commit

--- webtest/src/main/java/com/aufe/test/GitTest.java
+++ webtest/src/main/java/com/aufe/test/GitTest.java
@@ -3,5 +3,6 @@ package com.aufe.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("你好,小明");
     }
 }
